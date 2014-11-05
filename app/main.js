4fdad8689190fc1bde0652fa2faabc9cd837e535
CharisseL <charisse8@gmail.com>
2014-11-05 12:53:15 -0600

    test 1 passing, test 2 written

---
+++ app/main.js
@@ -0,0 +1,20 @@
+function nameCheck(name){
+	return 'Your name is ' + name + '!';
+}
+
+
+
+
+
+/*$('#li').click(function(){
+		cheerName($p, optionValue);
+	$(document).on('click', 'li', function(){
+	});*/
+
+
+
+//code for what's your name
+//
+//
+//
+//code for splitting string into 
---
+++ app/tests.js
@@ -0,0 +1,10 @@
+var q = QUnit;
+//what is your name?
+q.test('nameCheck(Risse)', function(assert) {
+	assert.equal(nameCheck('Risse'), 'Your name is Risse!');
+});
+
+q.test('nameCheer(name)', function(assert) {
+	expectedCheer = 'Give me a...R!\nGive me a ... I!\nGive me a ... S!\nGive me a ... S!\nGive me a ... E!'
+	assert.equal(nameCheer('RISSE'), expectedCheer);
+}
